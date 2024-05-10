22b44d9dd9448416ee0b4a52378b82b3d16c809d
Daniel Carmichael <danielfcarmichael@gmail.com>
2024-05-10 10:45:22 -0700

    modify auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (user) => {
   const payload = { userId: user._id };
-  const secret = process.env.SECRET_JWT;
+  const secret = process.env.JWT_SECRET;
   const options = { expiresIn: "7d" };
   const token = jwt.sign(payload, secret, options);
   return token;
