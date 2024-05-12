e676fae2714edc4179a14e4c2f1efc9b363ddfc8
Daniel Carmichael <danielfcarmichael@gmail.com>
2024-05-12 05:40:52 -0700

    console logging for error handling

--- routes/integrations/OpenAI.js
+++ routes/integrations/OpenAI.js
@@ -11,31 +11,40 @@ router.post("/generate-avatar", authMiddleware, async (req, res) => {
   const user_id = req.userId;
   const { gender, name, hairColor, avatarPrompt, useExisting = false } = req.body;
 
+  console.log(1)
   if (user_id) {
+    console.log(2)
     dbConnect(process.env.DB_CONNECTION_STRING);
 
     if ((gender, name, hairColor, avatarPrompt)) {
+        console.log(3)
       try {
         // Check if the user already exists
         const existingUser = await User.findOne({ user_id });
+        console.log(4)
         if (!existingUser) {
+            console.log(5)
           return res.status(404).json({ message: "Could not find account" });
         }
 
+        console.log(6)
         const existingAvatar = await Avatar.findOne({ owned_by_id: user_id });
 
         const openai = new OpenAI({
           organization: process.env.OPEN_AI_ORG,
           project: process.env.OPEN_AI_PROJECT,
         });
+        console.log(7)
 
         if (existingAvatar && useExisting) {
           // add existing configs to new configs
         } else {
+            console.log(8)
           const image = await openai.images.generate({
             model: "dall-e-3",
             prompt: `Generate an image of a character, with a white background, that is a ${gender} with a hair color of ${hairColor}. The character should be professional. Also, make the character ${avatarPrompt}`,
           });
+          console.log(9)
 
           const new_avatar = new Avatar({
             avatar_id: uuidv4(),
@@ -46,7 +55,11 @@ router.post("/generate-avatar", authMiddleware, async (req, res) => {
             owned_by_id: user_id
           });
 
+          console.log(10)
+
           await new_avatar.save();
+
+          console.log(11)
 
           res.status(200).json({
             message: "Avatar created",
