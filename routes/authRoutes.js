cb2c25b77b0f97bd30c0de2ebd0e14ccfeb2a4db
Daniel Carmichael <danielfcarmichael@gmail.com>
2024-05-12 06:06:16 -0700

    bug tracking

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,21 +9,32 @@ const uuidv4 = require("uuidv4");
 // Sign-up route
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
+  console.log(1)
 
   dbConnect(process.env.DB_CONNECTION_STRING);
+  console.log(2)
 
   try {
     // Check if the user already exists
     const existingUser = await User.findOne({ email });
+    console.log(3)
     if (existingUser) {
+      console.log(4)
       return res.status(400).json({ message: "User already exists" });
     }
 
     // Hash the password
     const hashedPassword = await hashPassword(password);
-
+    console.log(5)
+    console.log(uuidv4())
+    const user_id = uuidv4();
     // Create a new user
-    const newUser = new User({ user_id: uuidv4(), email, password: hashedPassword });
+    const newUser = new User({
+      user_id,
+      email,
+      password: hashedPassword,
+    });
+    console.log(6)
     await newUser.save();
 
     // Generate and return a JWT token
@@ -56,7 +67,7 @@ router.post("/login", async (req, res) => {
     // Generate and return a JWT token
     const token = generateToken(user);
 
-    console.log(token)
+    console.log(token);
     res.status(200).json({ token });
   } catch (err) {
     console.error(err);
